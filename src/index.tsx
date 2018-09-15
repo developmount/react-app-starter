8bfae062602d39a3a862766c965b093bae9b3ef9
Greg Chamberlain <gregchamberlain94@gmail.com>
2018-09-15 17:06:12 -0400

    Added webpack config, initial src/

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import App from './App';
+import React from 'react';
+import { render } from 'react-dom';
+
+const rootEl = document.getElementById('root');
+
+render(<App />, rootEl);
--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,32 @@
 const path = require('path');
 const webpack = require('webpack');
+
+module.exports = {
+  mode: 'development',
+  entry: 'src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx$?/,
+        exclue: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env', '@babel/react', '@babel/typescript'],
+            plugins: ['react-hot-loader/babel'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  devServer: {
+    port: 3000,
+  },
+};
