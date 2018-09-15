75d105eb9cb90bb7426bfe61edad7b1450a3ac48
Greg Chamberlain <gregchamberlain94@gmail.com>
2018-09-15 17:50:36 -0400

    Added common, dev, and prod webpack configs

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import App from './App';
 import React from 'react';
 import { render } from 'react-dom';
 
-const rootEl = document.getElementById('root');
+const rootEl = document.createElement('div');
+document.body.appendChild(rootEl);
 
 render(<App />, rootEl);
--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,8 @@
 const path = require('path');
-const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -23,10 +23,13 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'React App',
+    }),
+  ],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
-  devServer: {
-    port: 3000,
   },
 };
